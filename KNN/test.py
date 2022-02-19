8b1795e3a8045006fece3c7911db2d31f6201212
Sebastian Bugal <sebastianbugal@Sebastians-MBP.hitronhub.home>
2022-02-18 19:38:21 -0800

    testing upgrades

--- KNN/test.py
+++ KNN/test.py
@@ -1,4 +1,6 @@
+from cProfile import label
 from hashlib import new
+from pdb import post_mortem
 from turtle import title
 from sklearn.datasets import make_blobs
 from pandas import DataFrame
@@ -11,20 +13,17 @@ if __name__ == "__main__":
     X, y = make_blobs(n_samples=200, centers=2, n_features=2)
     random_index = random.randrange(0,len(X)-1)
     point = X[random_index]
+    real_label = y[random_index]
     X = np.delete(X, random_index, axis=0)
+    y = np.delete(y, random_index, axis=0)
     knn = k_nn(X,y)
-    knn.predict(point)
-    # df = DataFrame(dict(x=X[:,0], y=X[:,1], label=y))
-    # colors = {0:'red', 1:'blue', 2:'yellow'}
-    # fig, ax = pyplot.subplots()
-    # grouped = df.groupby('label')
-
-    # for key, group in grouped:
-    #     group.plot(ax=ax, kind='scatter', title = 'Actual', x='x', y='y', label=key, color=colors[key]) 
-    # # pyplot.show()
-
-    # fig2, ax2 = pyplot.subplots()
-    # grouped2 = df_test.groupby('label')
-    # for key, group in grouped2:
-    #     group.plot(ax=ax2, kind='scatter',title = 'Test', x='x', y='y', label=key, color=colors[key]) 
-    # pyplot.show()
+    pred_class = knn.predict(point)
+    print(f'Predicted Class: {pred_class}, Actual Class: {real_label}')
+    df = DataFrame(dict(x=X[:,0], y=X[:,1], label=y))
+    colors = {0:'red', 1:'blue', 2:'yellow'}
+    fig, ax = pyplot.subplots()
+    grouped = df.groupby('label')
+    for key, group in grouped:
+        group.plot(ax=ax, kind='scatter', title = 'Actual', x='x', y='y', label=key, color=colors[key]) 
+    pyplot.plot(point[0], point[1], marker="v", markersize=10, markeredgecolor=colors[real_label], color=colors[pred_class])
+    pyplot.show()
